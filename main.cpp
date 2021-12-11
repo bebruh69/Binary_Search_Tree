696708b6de9a351d917c7b138f02be063800c5fa
bebruh69 <aprilcheetah@gmail.com>
2021-12-11 15:45:18 +0300

    Added: BST class

--- main.cpp
+++ main.cpp
@@ -1,9 +1,15 @@
-#include <iostream>
+#include<iostream>
+#include<binary_search_tree.h>
 
 using namespace std;
 
-int main()
-{
-    cout << "Hello World!" << endl;
+int main() {
+    BST t;
+    t.insert(20);
+    t.insert(25);
+    t.insert(15);
+    t.insert(10);
+    t.insert(30);
+    t.display(0);
     return 0;
 }
